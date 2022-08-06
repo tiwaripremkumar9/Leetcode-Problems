4a80119f1c98f9b391cfc5637aa5af2e50b43ad8
u726608 <prem.tiwari@finastra.com>
2022-08-06 23:45:59 +0530

    Intializing file

---
+++ Description.java
@@ -0,0 +1,9 @@
+/**
+ * Description
+ */
+public class Description {
+
+    public static void main(String[] args) {
+        System.out.println("I solve leetcode problems and put it here for revision and practics purpose");
+    }
+}

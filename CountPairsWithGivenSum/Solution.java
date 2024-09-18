4b687cf9da3bb7f44ed043d920bd2f101b633111
Prem Tiwari <tiwaripremkumar9@gmail.com>
2024-09-18 10:29:31 +0530

    Count Pairs with given sum

---
+++ CountPairsWithGivenSum/Solution.java
@@ -0,0 +1,28 @@
+package CountPairsWithGivenSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, -1, 5, -2, 8};
+        int target = 6;
+        int count = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            int complement = target - arr[i];
+            if(map.containsKey(complement)){
+                count++;
+            }
+            else{
+                map.put(arr[i], complement);
+            }
+
+        }
+
+        System.out.println(count);
+    }
+    
+}

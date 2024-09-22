f585d700c36fdf5e3742b96a9968c19f32eb36f0
Prem Kumar Tiwari <tiwaripremkumar9@gmail.com>
2024-09-22 22:14:35 +0530

    SubarrayWithZeroSum - O(n) Approach using Hashmap

--- SubarrayWithZeroSum/Solution.java
+++ SubarrayWithZeroSum/Solution.java
@@ -19,15 +19,47 @@ public class Solution {
                     resultArray[1] = j;
                     break;      
                 }
-               
             }
         }
         return resultArray;
     }
+
+    public static List<int[]> findZeroSumSubarrays(int[] nums) {
+        // Create a HashMap to store the cumulative sum and corresponding indices
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        List<int[]> result = new ArrayList<>();
+        int sum = 0;
+
+        // Initialize the map with sum 0 at index -1
+        map.put(0, new ArrayList<>());
+        map.get(0).add(-1);
+
+        // Traverse the array
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            // If the sum is seen before, it means there is a zero-sum subarray
+            if (map.containsKey(sum)) {
+                for (int start : map.get(sum)) {
+                    result.add(new int[]{start + 1, i});
+                }
+            }
+
+            // Add the current sum to the map
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(i);
+        }
+
+        return result;
+    }
+
     
 
     public static void main(String[] args) {
         int [] arr = {4,5,2,1,3,-6, 9,7,6};
-        System.out.println(Arrays.toString(bruteForce(arr)));
+        //System.out.println(Arrays.toString(bruteForce(arr)));
+        List<int[]> resultList = findZeroSumSubarrays(arr);
+        resultList.forEach(each -> System.out.println(Arrays.toString(each)));
+
     }
 }

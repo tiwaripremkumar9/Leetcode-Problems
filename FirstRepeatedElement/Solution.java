58756854cda63d5621fb171ea24f9ad434bd138d
Prem Kumar Tiwari <tiwaripremkumar9@gmail.com>
2024-09-22 20:54:49 +0530

    Using Streams

--- FirstRepeatedElement/Solution.java
+++ FirstRepeatedElement/Solution.java
@@ -28,7 +28,24 @@ class Solution {
         }
 
         System.out.println(result);
+        usingStreams();
 
+    }
+
+    /*
+     * .filter(n -> !seen.add(n)) filters the stream to include only those elements that are already in the seen set. 
+     * The add method of HashSet returns false if the element is already present, so !seen.add(n) will be true for duplicates.
+     */
+
+    public static void usingStreams(){
+        int [] arr = {1,2,3,4,5,5,6,7,7,7,8,1,1,4,8};
+        Set<Integer> set = new HashSet<>();
+        int result = Arrays.stream(arr)
+                            .filter(each -> !set.add(each))
+                            .findFirst()
+                            .orElse(-1);
+        System.out.println("********** Using Streams ***********");
+        System.out.println(result);
     }
     
 }

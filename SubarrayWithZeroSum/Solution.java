051bf6a17abeb45cb3bf38a9d0545f16fd0767b7
Prem Tiwari <tiwaripremkumar9@gmail.com>
2024-09-23 19:54:18 +0530

    rewriting the solution

--- SubarrayWithZeroSum/Solution.java
+++ SubarrayWithZeroSum/Solution.java
@@ -24,42 +24,33 @@ public class Solution {
         return resultArray;
     }
 
-    public static List<int[]> findZeroSumSubarrays(int[] nums) {
-        // Create a HashMap to store the cumulative sum and corresponding indices
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        List<int[]> result = new ArrayList<>();
+    public static int[] findZeroSumSubarray(int[] arr) {
         int sum = 0;
-
-        // Initialize the map with sum 0 at index -1
-        map.put(0, new ArrayList<>());
-        map.get(0).add(-1);
-
-        // Traverse the array
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-
-            // If the sum is seen before, it means there is a zero-sum subarray
-            if (map.containsKey(sum)) {
-                for (int start : map.get(sum)) {
-                    result.add(new int[]{start + 1, i});
-                }
+        int [] resultArray = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
             }
-
-            // Add the current sum to the map
-            map.putIfAbsent(sum, new ArrayList<>());
-            map.get(sum).add(i);
+            else{
+                resultArray[0] = map.get(sum) + 1;
+                resultArray[1] = i;
+                break;
+            }
         }
 
-        return result;
+        return resultArray;
+       
     }
 
     
 
     public static void main(String[] args) {
-        int [] arr = {4,5,2,1,3,-6, 9,7,6};
+        int [] arr = {4,5,2,1,3,-6,9,7,6};
         //System.out.println(Arrays.toString(bruteForce(arr)));
-        List<int[]> resultList = findZeroSumSubarrays(arr);
-        resultList.forEach(each -> System.out.println(Arrays.toString(each)));
-
+        int [] resultArray = findZeroSumSubarray(arr);
+        //resultList.forEach(each -> System.out.println(Arrays.toString(each)));
+        System.out.println(Arrays.toString(resultArray));
     }
 }

0f9f9779a355062ac658bb06fb4627deadc24e8f
Prem Kumar Tiwari <tiwaripremkumar9@gmail.com>
2024-09-24 22:04:27 +0530

    Final solution

--- MaxLengthSubarrayWithGivenSum/Solution.java
+++ MaxLengthSubarrayWithGivenSum/Solution.java
@@ -16,19 +16,26 @@ public class Solution {
                 map.put(sum, i);
             }
             else{
-                int length = i - (map.get(sum) + 1);
+            	//find the length of the sub array 
+                int length = i - (map.get(complementSum) + 1);
+                //if the current subarray is the max length 
                 if(length > maxLength){
                     maxLength = length;
-                    resultArray[0] = map.get(sum) + 1;
+                    resultArray[0] = map.get(complementSum) + 1;
                     resultArray[1] = i;
                 }
+                /*
+                 * if it is not the max length subrray with given sum,
+                 *  then iterate ahead in search of another possible max length subarray
+                 */
+                map.put(sum, i);
             }
         }
         return resultArray;
     }
 
     public static void main(String[] args) {
-        int [] arr = {1, 2, 3, 7, 5};
+        int [] arr = {1, 2, 3, 7, 5, 1, 4, 3, 4, 6,6};
         System.out.println(Arrays.toString(maxLengthSubarrayWithGivenSum(arr, 12)));
 
         

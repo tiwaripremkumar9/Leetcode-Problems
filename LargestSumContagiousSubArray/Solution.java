96056e2d76ea450917574b1a687c7900326788b7
Prem Kumar Tiwari <tiwaripremkumar9@gmail.com>
2024-09-25 21:50:36 +0530

    Kadane's Algortihm Approach

--- LargestSumContagiousSubArray/Solution.java
+++ LargestSumContagiousSubArray/Solution.java
@@ -2,6 +2,22 @@ package LargestSumContagiousSubArray;
 
 public class Solution {
 
+    public static int kadanesAlgortihmApproach(int [] arr){
+    	int currentSum = 0;
+    	int maxSum = 0;
+    	
+    	for(int i = 0; i < arr.length; i++) {
+    		currentSum += arr[i];
+    		if(currentSum < 0) {
+    			currentSum = 0;
+    		}
+    		if(currentSum > maxSum) {
+    			maxSum = currentSum;
+    		}
+    	}
+    	return maxSum;
+    }
+
     public static int bruteForce(int [] arr) {
     	int currentSum = 0;
     	int maxSum = 0;
@@ -21,7 +37,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int [] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println((bruteForce(arr)));
+        System.out.println((kadanesAlgortihmApproach(arr)));
     }
     
 }

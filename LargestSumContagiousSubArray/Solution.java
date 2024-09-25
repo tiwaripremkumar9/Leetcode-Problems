e166958ef084680ccbf44e2aed120abe2b1c2044
Prem Kumar Tiwari <tiwaripremkumar9@gmail.com>
2024-09-25 22:54:20 +0530

    Kadane's Algo for all negative values in array

--- LargestSumContagiousSubArray/Solution.java
+++ LargestSumContagiousSubArray/Solution.java
@@ -2,6 +2,10 @@ package LargestSumContagiousSubArray;
 
 public class Solution {
 
+    /*
+     * below code of Kadane's doesn't works as expected when all the values of array are negative
+     */
+
     public static int kadanesAlgortihmApproach(int [] arr){
     	int currentSum = 0;
     	int maxSum = 0;
@@ -16,6 +20,26 @@ public class Solution {
     		}
     	}
     	return maxSum;
+    }
+
+    /*
+     * Kadane's algorithm idea lies in the fact tht during iteration and summing, we should try to find the larger value possible,
+     * hence, if sum till a[n] > a[i], then sum till a[n] is largely helpful for getting the max sum moving to further elements.
+     * thinking of vice-versa, if a[i] > sum till a[n] then a[i] is largely helpful for getting the max sum moving to further elements.
+     * 
+     * this way, we keep removing the lowest negative values (depereciating factor in our sum), and in way if we try finding the maximum sum.
+     */
+   
+     public static int refinedKadanesAlgorithm(int [] arr){
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+
+        return maxSum;
     }
 
     public static int bruteForce(int [] arr) {
@@ -37,7 +61,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int [] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println((kadanesAlgortihmApproach(arr)));
+        System.out.println((refinedKadanesAlgorithm(arr)));
     }
     
 }
